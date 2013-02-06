ad0eba2debbe746a4b6a4cb5f7c51fc491b2843c
Greg Hale <ImAlsoGreg@gmail.com>
2013-02-06 15:47:59 -0500

    haskell setup

---
+++ backend/src_haskell/DataSource.hs
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------
+-- |
+-- Module     : DataSource.Main
+-- Copyright  : (c) Greg Hale 2013
+-- License    : GPL3
+-- 
+-- Maintainer : imalsogreg@gmail.com
+-- Stability  : unstable
+-- Portability: not portable, uses posix & linux kernel modules
+--
+-- ArteBackend, a headless interface to nidaq cards for arte-ephys
+--
+----------------------------------------------------------------------
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello test"
