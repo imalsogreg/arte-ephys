d4f84cc4a14863d487249549e921703b69c4a58a
Greg Hale <ImAlsoGreg@gmail.com>
2013-03-04 09:52:06 -0500

    zmq starting

--- ArteMaster/Master.hs
+++ ArteMaster/Master.hs
@@ -6,7 +6,7 @@
 -- 
 -- Maintainer : imalsogreg@gmail.com
 -- Stability  : unstable
--- Portability: not portable, uses posix & linux kernel modules
+-- Portability: not portable. Tries to launch 
 --
 -- ArteMaster, A user interface for issuing commands to Arte
 --
@@ -14,6 +14,7 @@
 
 module Main where
 
+import System.ZMQ as Z
 
 main :: IO ()
 main = undefined
@@ -22,9 +23,19 @@ main = undefined
 -- (Startup Vis, Tracker on other machine?)
 -- Ask Backend for backend configuration
 -- Setup command server
--- Letup listeners to other servers
+-- Setup listeners to other servers?
 -- Setup GUI
 -- Poll for ZMQ events and UI events
 -- Display received messages
 -- Send messages in response to UI (eg, start/stop acq.
 --        reset clocks, set 
+
+type TransportName   = String
+type HostName        = String
+type PortName        = String
+type ZmqSocketString = String
+
+zmqSocketString :: TransportName -> HostName -> PortName -> ZmqSocketString
+zmqSocketString t h p = t ++ "://" ++ h ++ ":" ++ p
+
+setupCommandServer :: TransportName -> HostName -> PortName -> IO (Z.Req)
