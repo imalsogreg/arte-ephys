2067037b538112c3efa07e6701f563d80eec78a4
Greg Hale <imalsogreg@gmail.com>
2013-10-17 18:29:24 -0400

    lawn chairs

--- ArteCommon/src/Arte/Common.hs
+++ ArteCommon/src/Arte/Common.hs
@@ -12,7 +12,7 @@
 --
 ----------------------------------------------------------------------
 
-module ArteBase where
+module Common where
 
 import System.ZMQ
 import ZmqUtils
@@ -54,4 +54,18 @@ connectToMasterServer ctx masterStr = do
   connect cliSocket masterStr
   return cliSocket
   
-    
+
+
+type ZmqHost    = String
+type ZmqPort    = String
+type ZmqSockStr = String
+data ZmqProtocol = Tcp | Ipc deriving (Eq)
+
+instance Show ZmqProtocol where
+  show Tcp = "tcp"
+  show Ipc = "ipc"
+
+zmqStr :: ZmqProtocol -> ZmqHost -> ZmqPort -> ZmqSockStr
+zmqStr prot host port = 
+  show prot ++ "://" ++ host ++ ":" ++ port
+  
