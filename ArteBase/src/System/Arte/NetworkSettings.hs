d84eec582121c1eda484d309e63500388fcf755f
Greg Hale <ImAlsoGreg@gmail.com>
2013-10-17 17:37:17 -0400

    mock spikes. brainstorming

--- ArteBase/src/System/Arte/NetworkSettings.hs
+++ ArteBase/src/System/Arte/NetworkSettings.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module System.Arte.NetworkSettings where
 
 import Data.Text
@@ -6,26 +8,20 @@ import Control.Monad
 import Data.Yaml
 
 data Host = Host 
-            { hostName :: String
+            { hostName :: Text
             , hostIP   :: String
             } deriving (Show)
                        
 data Node = Node 
-            {  nodeName :: String
+            {  nodeName :: Text
             ,  nodeHost :: Host
             ,  nodePort :: String
             }
 
-data NetworkSettings = NetworkSettings {
-    networkHosts :: [Host]
-  , networkNodes :: [Node]
-  }
-
-
 instance FromJSON Host where
   parseJSON (Object v) = Host <$> 
-                         v .: pack "name" <*>
-                         v .: pack "ip"
+                         v .: "name" <*>
+                         v .: "ip"
   parseJSON _  = mzero
   
 instance FromJSON Node where
@@ -34,12 +30,3 @@ instance FromJSON Node where
                          v .: pack "host" <*>
                          v .: pack "port"
   parseJSON _          = mzero
-
-
-                       
-instance FromJSON NetworkSettings where
-  parseJSON (Object v) = NetworkSettings <$>
-                         v .: pack "hosts" <*>
-                         v .: pack "nodes"
-  parseJSON _          = mzero
-  
--- ArteMockSpikes/src/ArteMockSpikes.hs
+++ ArteMockSpikes/src/ArteMockSpikes.hs
@@ -1,4 +1,22 @@
 module Main where
 
-import 
+import System.Arte.ArteBase
+
+import qualified System.ZMQ as ZMQ
 import qualified Pipes as PP
+import Data.Yaml
+import System.Directory
+import System.Environment
+import System.Console.CmdArgs
+
+data CArgs = CArgs { mwlExtensions  :: [String]
+                   , arteExtensions :: [String]
+                   , baseNames      :: Maybe [String]
+                   , 
+
+main :: IO ()
+main = do
+  (Just netSettings) <- parse =<< readFile (getEnv "HOME")
+  let (Just spikeSettings) = lookup netSettings "spkes"
+      (myHostname myPortnum) = Vector.head spikeSettings
+  fileNames <- 
