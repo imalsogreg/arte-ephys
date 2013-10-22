2f9ed6ea0f0b749af9fcfda477802381a5e1754a
Greg Hale <imalsogreg@gmail.com>
2013-10-22 15:59:54 -0400

    Got things working with simplified node type - no need to filtered

--- ArteCommon/src/Arte/Common/Network.hs
+++ ArteCommon/src/Arte/Common/Network.hs
@@ -3,7 +3,6 @@
 module Arte.Common.Network where
 
 import Data.Text hiding (unwords)
-import Data.Maybe (catMaybes)
 import Control.Applicative
 import Control.Monad
 import Control.Lens hiding ((.=))
@@ -11,73 +10,56 @@ import Data.Aeson
 import Data.Yaml
 import Data.Aeson.Lens
 import qualified Data.ByteString.Char8 as BS
-import Control.Monad.Trans.Class
-import qualified Data.HashMap.Strict as HashMap
-
-import Data.Maybe
+import Data.Map (Map, keys, member)
 
 type HostName = String
 type IPAddy   = String
 type Port     = String
-{-
-getAppNode :: String -> String -> FilePath -> IO (Either String Node)
-getAppNode nodeType appName fn = do
+
+getAppNode :: String -> FilePath -> IO (Either String Node)
+getAppNode nodeName fn = do
   f <- BS.readFile fn
   let v =  Data.Yaml.decode f :: Maybe Value
-  case v ^. key (pack nodeType) :: Maybe [Value] of
-    Nothing -> return $ Left ("No node type " ++ nodeType)
-    Just nodes ->
-      case Prelude.filter (\n -> nodeName n == (pack appName)) nodes of
-        [] -> return $ Left (unwords ["Couldn't find",appName,"in",nodeType])
-        (m:atches) -> return $ Right m
--}
+  case v ^. key "nodes" . key (pack nodeName) :: Maybe Node of
+    Just node -> return $ Right node
 
-a :: Node
-a = Node "sampleNodeName" (Host "HostName" "HostIP") "nodePort"
-
-getF :: IO BS.ByteString
-getF = BS.readFile "/home/greghale/.arte-ephys/network.conf"
-
-getV :: IO (Maybe Value)
-getV = Data.Yaml.decode `liftM` getF
-
-getObjs :: IO [Value]
-getObjs = do
-  v <- getV
-  return $ v ^. key "spikes" . folded :: IO [Value]
+  -- Error handling
+    Nothing->
+      case v of
+        Nothing -> return $ Left ("Bad conf file: " ++ fn)
+        _ -> case v ^. key "nodes" :: Maybe (Map String Node) of
+          Nothing      ->
+            return $ Left ("Malformed conf file lacks nodes: field")
+          Just nodeMap -> case member nodeName nodeMap of
+            True  -> return $ Left ("No parse for node " ++ nodeName)
+            False -> return $ Left
+                     ("Node '" ++ nodeName ++ "' not in node list: " ++
+                      (unwords . keys  $ nodeMap))
 
 data Host = Host 
-            { hostName :: String
-            , hostIP   :: String
+            { hostIP   :: String
             } deriving (Eq, Show)
                        
 data Node = Node 
-            {  nodeName :: String
-            ,  nodeHost :: Host
-            ,  nodePort :: String
+            {  nodeHost :: Host
+            ,  nodePort :: Int
             } deriving (Eq, Show)
 
 instance FromJSON Host where
-  parseJSON (Object v) = Host <$> 
-                         v .: "name" <*>
-                         v .: "ip"
+  parseJSON (Object v) = Host <$> v .: "ip"
   parseJSON _  = mzero
   
 instance FromJSON Node where
   parseJSON (Object v) = Node <$>
-                         v .: "name" <*>
                          v .: "host" <*>
                          v .: "port"
   parseJSON _          = mzero
 
 instance ToJSON Host where
-  toJSON (Host hName hIP) =
-    object ["name" .= hName
-           ,"ip"   .= hIP
-           ]
+  toJSON (Host hIP) =
+    object ["ip"   .= hIP]
 
 instance ToJSON Node where
-  toJSON (Node nName nHost nPort) =
-    object ["name" .= nName
-           ,"host" .= nHost
+  toJSON (Node nHost nPort) =
+    object ["host" .= nHost
            ,"port" .= nPort]
