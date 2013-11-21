11457135d4ced5022f937972fbac5ddf5c40d2d7
Greg Hale <imalsogreg@gmail.com>
2013-11-21 09:33:36 -0500

    handle addition of clusters in decoder

--- ArteDecode/src/ArteDecode.hs
+++ ArteDecode/src/ArteDecode.hs
@@ -3,7 +3,6 @@ module Main where
 import Arte.Common.Net
 import Arte.Common.NetMessage
 import Data.Ephys.Spike
-import Data.Ephys.Spike
 import Data.Ephys.Cluster
 import Data.Ephys.PlaceCell
 
@@ -18,9 +17,50 @@ getAllSpikeNodes configFilePath =
   (\l -> getAppNode ("spikes" ++ [l]) configFilePath) >>= \nodes' ->
   return $ rights nodes'
 
---   All tetrodes       Name         ClustName AccumField'     ClusterBound'
---   |                  |            |         |               |
-type Tetrodes = Map.Map Int (Map.Map Int (TVar PlaceCell, TVar ClusterMethod))
+type SpikeHistory = Int -- Placeholder.  Will be more like: 
+
+newPlaceCell :: Track
+             -> TVar Tetrodes
+             -> TrodeName
+             -> ClusterMethod
+             -> IO (Maybe PlaceCell)
+newPlaceCell track tetrodesV t cMethod = do
+  tetrodes <- atomically $ readTVar tetrodesV
+  case Map.lookup tetrodes t of
+    Nothing -> return Nothing
+    Just (_, spikeHistoryV) -> do
+      return . Just $
+        PlaceCell cMethod (Map.fromList [(p,0)|p <- allTrackPos track])
+        -- TODO: Build from spike history!
+
+setTetrodeCluster :: Track
+                  -> TVar Tetrodes 
+                  -> TrodeName
+                  -> ClusterName
+                  -> IO ()
+setTetrodeClusters track tetrodesV t c clustMap = do
+  tetrodes <- atomically $ readTVar tetrodesV  -- atomically here, happy to
+  let tetrode' = case Map.lookup tetrodes t of     -- drop a few spikes while 
+        Just (placeCellsV,sHistoryV) -> do     -- updating cluster definition
+          (placeCells,sHistory) <- atomically $ do
+            p <- readTVar placeCellsV
+            s <- readTVar sHistoryV
+            return (p,s)
+          case Map.lookup placeCells c of
+            Nothing ->
+              let newCell = 
+          
+  
+type TetrodeName = Int
+type ClusterName = Int
+
+type Tetrode = (TVar (Map.Map Int PlaceCell), TVar SpikeHistory)
+
+type Tetrodes = Map.Map TetrodeName Tetrode
+
+handleRequests :: TQueue ArteMessage -> TVar Tetrodes -> IO ()
+handleRequests queue tetrodes = do
+  (ArteMessage t nFrom nTo mBody) <- atomically $ readTQueue queue
 
 main :: IO ()
 main = do
