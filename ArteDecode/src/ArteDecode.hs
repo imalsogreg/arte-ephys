65e9367193b80d7bb9a629e64ddf8ebb958a992e
Greg Hale <imalsogreg@gmail.com>
2013-11-20 23:56:10 -0500

    little skeleton decoding stuf

--- ArteDecode/src/ArteDecode.hs
+++ ArteDecode/src/ArteDecode.hs
@@ -1,14 +1,29 @@
-module Arte.Decode where
+module Main where
 
-import Data.Ephys.Net
-import Data.Ephys.NetMessage
+import Arte.Common.Net
+import Arte.Common.NetMessage
 import Data.Ephys.Spike
-import Data.Ephys.TrodeSpike
+import Data.Ephys.Spike
+import Data.Ephys.Cluster
+import Data.Ephys.PlaceCell
 
+import qualified Data.Map as Map
+import Control.Monad
 import Control.Concurrent.STM
+import Data.Either
+
+getAllSpikeNodes :: Maybe FilePath -> IO [Node]
+getAllSpikeNodes configFilePath = 
+  forM  ['A'..'Z'] 
+  (\l -> getAppNode ("spikes" ++ [l]) configFilePath) >>= \nodes' ->
+  return $ rights nodes'
+
+--   All tetrodes       Name         ClustName AccumField'     ClusterBound'
+--   |                  |            |         |               |
+type Tetrodes = Map.Map Int (Map.Map Int (TVar PlaceCell, TVar ClusterMethod))
 
 main :: IO ()
 main = do
   masterNode' <- getAppNode "master" Nothing
-  spikeNodes  <- getAllSpikeNodes -- TODO define this here or in Net.hs
-  
+  spikeNodes  <- getAllSpikeNodes    Nothing
+  print "Ok"
