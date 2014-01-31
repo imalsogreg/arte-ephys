a7e18518e2cfb0068f82b9fce164899bb245f514
Greg Hale <imalsogreg@gmail.com>
2014-01-30 23:45:22 -0500

    rebuilt deps with --enable-library-profiling and programs with --enable-executabel-profiling.  Ran programs independently instead of with script, generated .prof files.

--- scripts/simpleDemo.sh
+++ scripts/simpleDemo.sh
@@ -4,10 +4,10 @@ DATADIR="/home/greghale/Data/caillou/112812clip2"
 STARTTIME="4492"
 
 echo "Starting ArteMaster"
-ArteMaster/.cabal-sandbox/bin/arte-master -s=2 &
+ArteMaster/.cabal-sandbox/bin/arte-master -s=2 +RTS -p &
 
 echo "Starting ArteDecode"
-ArteDecode/.cabal-sandbox/bin/arteDecode &
+ArteDecode/.cabal-sandbox/bin/arteDecode +RTS -p &
 
 echo "Starting mock data"
-ArteMockMWL/.cabal-sandbox/bin/arteMockSpikes -i=false --startexperimenttime=$STARTTIME --basedirectory=$DATADIR --searchdepth=2 &
+ArteMockMWL/.cabal-sandbox/bin/arteMockSpikes -i=false --startexperimenttime=$STARTTIME --basedirectory=$DATADIR --searchdepth=2 +RTS -p &
