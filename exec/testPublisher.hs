5a7c478f9009d7bce5bc0dd7b4d144d8ed1eeb72
Greg Hale <imalsogreg@gmail.com>
2014-05-16 22:03:08 -0400

    optparse for test program

--- exec/testPublisher.hs
+++ exec/testPublisher.hs
@@ -10,30 +10,54 @@ import Control.Concurrent.Async
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TQueue
 import Control.Monad
+import Data.Maybe
+import Options.Applicative
 
-h :: Host
-h = Host "ThisHost" "10.121.43.167"
+data Opts = Opts
+            { server :: Bool
+            , client :: Bool
+            , host   :: String
+            , port   :: String
+            }
 
-node :: String -> Node
-node s = Node s h 8001
+opts :: Parser Opts
+opts = Opts
+       <$> switch
+       ( long "server" <> short 's' <> help "Run as server")
+       <*> switch
+       ( long "client" <> short 'c' <> help "Run as client")
+       <*> strOption
+       ( long "host" <> short 'h' <> help "Hostname")
+       <*> strOption
+       ( long "port" <> short 'p' <> help "Port number")
+       
+node :: String -> String -> Node
+node h p = Node h (Host h h) (read p)
 
-main :: IO ()
-main = do
-  
-  args <- getArgs
-  case args of
-    ["server"] -> do
+run :: Opts -> IO ()
+run (Opts True False h p) = serve h p
+run (Opts False True h p) = cli   h p
+run _ = error "Must run as either server or client"
+
+serve :: String -> String -> IO ()
+serve h p = do
       q <- atomically $ newTQueue
       pub <- atomically $ DataPublisher q <$> newTVar []
-      pubA <- async $ acceptSubscribers (node "MeServer") pub
+      pubA <- async $ acceptSubscribers (node h p) pub
       forM_ [(1::Int)..] $ \n -> do
         print $ "Writing to queue " ++ show n
         atomically (writeTQueue q n)
         threadDelay 500000
       wait pubA
-    ["client"] -> do
-      withSubscription (node "ServerNode") $ \a ->
+
+cli :: String -> String -> IO ()
+cli h p = do
+      withSubscription (node h p) $ \a ->
         case (a :: Either String Int) of
           Left e           -> print $ "Error: " ++ e
           Right x -> print $ show x
-    _ -> error "Pass either server or client argument"
+
+main :: IO ()
+main = execParser opt >>= run
+  where opt = info (helper <*> opts)
+         (fullDesc <> progDesc "Test" <> header "Test")
