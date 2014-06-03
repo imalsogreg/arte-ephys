1e71bd3433216a77eae6e23d4148881fed5dc2b5
Greg Hale <ImAlsoGreg@gmail.com>
2014-06-03 11:11:53 -0400

    Wrestling with type system over polymorphic publisher...

--- src/System/Arte/MockData.hs
+++ src/System/Arte/MockData.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable, NoMonomorphismRestriction, TemplateHaskell #-}
+{-# LANGUAGE DeriveDataTypeable, NoMonomorphismRestriction, TemplateHaskell, TupleSections, OverloadedStrings #-}
 
 module Main where
 
@@ -30,6 +30,7 @@ import Control.Error
 import Control.Monad.Trans.Writer.Strict
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString as BS
+import Data.Configurator hiding (prefix)
 import qualified Data.Configurator as Conf
 import qualified Data.Configurator.Types as Conf
 import Data.Time.Clock
@@ -41,18 +42,20 @@ import System.FilePath ((</>))
 
 --import Control.Concurrent.STM.TQueue
 import Control.Monad
-import Data.Text (Text,pack,unpack)
+import Data.Text hiding (reverse, dropWhile, drop)
+import qualified Data.Text as T hiding (reverse, dropWhile, drop)
 import qualified Data.Map as M
 import qualified Data.Serialize as S
 import Control.Lens
 
 data MockStatus = Seeking | Waiting | Streaming
-                deriving (Eq, Show)
+                deriving (Eq, Show, Read)
 
 data DataFormat = JSON | Binary
-                deriving (Eq, Show)
+                deriving (Eq, Show, Read)
 
 data InputFileType = MwlEEG | MwlTT | MwlP
+                   deriving (Eq, Show, Read)
 
 data DataSourceOpts = DataSourceOpts {
     _dsNode      :: Node
@@ -66,7 +69,6 @@ data DataSource a = DataSource {
     _dsProducers :: [P.Producer a IO ()]
   , _dsWaiting   :: TVar (M.Map ExperimentTime a)
   , _dsPub       :: DataPublisher a
-  , _dsGetTS  :: (a -> ExperimentTime)
   }
 $(makeLenses ''DataSource)
 
@@ -89,55 +91,119 @@ $(makeLenses ''MockState)
 
 ------------------------------------------------------------------------------
 getSourceOpts :: Conf.Config -> IO [DataSourceOpts]
-getSourceOpts conf = catMaybes <$> forM [0..9] $ \i -> 
-  runMaybeT $ do
-    let prefix = (("source" ++ show i ++ ".") ++)
-    dp    <- Conf.lookup conf $ prefix "dataPort"
-    let node = Node (Host prefix "localhost")
-               (PortNumber . fromIntegral . read $ dp)
-    files <- Conf.lookup conf $ prefix "files"
-    fType <- Conf.lookup conf $ prefix "fileType"
-    fmt   <- Conf.lookup conf $ prefix "outFormat"
-    fp    <- lift $ processPath  files
-    return $ DataSourceOpts node fmt fp  fType
+getSourceOpts conf = catMaybes <$> sources
+  where sources =
+          forM [(0::Int)..9] $ \i -> do
+           let prefix = T.pack . (("source" ++ show i ++ ".") ++)
+           dp' <- Conf.lookup conf $ prefix "dataPort"
+           files <- Conf.lookup conf $ prefix "files"
+           fType <- Conf.lookup conf $ prefix "fileType"
+           fmt   <- Conf.lookup conf $ prefix "outFormat"
+           bp    <- Conf.lookup conf "dataBasePath"
+           case (dp', files, fType, fmt,bp) of
+             (Just p, Just fl, Just fT, Just fM, Just basePath) -> do
+               let node = Node "source"
+                          (Host (T.unpack $ prefix "host") "localhost")
+                          (fromInt . read $ p)
+               expPaths <- processPath (basePath ++ fl)
+               return . Just $ DataSourceOpts node (read fM) expPaths (read fT)
+             _ -> return Nothing
 
-getMockOpts :: FilePath -> IO (Either String MockState)
+getMockOpts :: FilePath -> IO (Either String MockOpts)
 getMockOpts f = do
   config <- Conf.load [Conf.Required f]
   runEitherT $ do
-    seekWait  <- Conf.lookup config "waitAfterSeeking"
-    lookAhead <- lookup config "readAheadSeconds"
-    basePath  <- lookup config "dataBasePath"
-    cmdPort   <- lookup config "commandPort"    
-    return $ MockOpts (Node (Host "MockData" "localhost")
-                       (PortNumber . fromIntegral . read $ cmdPort))
+    seekWait  <- noteT "Missing wait option" . MaybeT $
+                 Conf.lookup config "waitAfterSeeking"
+    lookAhead <- noteT "Missing look ahead" . MaybeT $
+                 Data.Configurator.lookup config "readAheadSeconds"
+--    basePath  <- noteT "Missing base directory" . MaybeT $
+--                 Data.Configurator.lookup config "dataBasePath"
+    cmdPort   <- noteT "Missing command port" . MaybeT $
+                 Data.Configurator.lookup config "commandPort"
+    seekTime  <- noteT "Missing default seek time" . MaybeT $
+                 Data.Configurator.lookup config "initialSeekTime"
+    return $ MockOpts
+      (Node "commandNode" (Host "MockData" "localhost") 
+       (fromInt . read $ cmdPort))
+      (read lookAhead) seekWait (read seekTime)
+
+fromInt :: Integral a => Int -> a
+fromInt = fromIntegral
 
 main :: IO ()
 main = do
   undefined
 
+class Timestamp a where
+  getTS :: a -> ExperimentTime
 
+instance Timestamp TrodeSpike where
+  getTS = spikeTime
+
+instance Timestamp Position where
+  getTS = _posTime
+
+data AnyDataSource = DSSpikes (DataSource TrodeSpike)
+                   | DSPos    (DataSource Position)
 
 ------------------------------------------------------------------------------
-mkSource :: MockOpts -> DataSourceOpts -> IO (DataSource a)
-mkSource mock source = do
+mkSource :: Conf.Config -> MockOpts -> DataSourceOpts -> IO (AnyDataSource)
+mkSource cfg mock source = do
   q <- atomically . newTVar $ M.empty
+  s <- atomically . newTVar $ Seeking
   p <- DataPublisher <$> (atomically $ newTQueue) <*> newTVarIO []
-  let timeFun = case (source^.dsFileType) of
-        MwlTT -> spikeTime
-        MwlP  -> posTime
-      node = source ^. dsNode
+  case (source^.dsFileType) of
+    MwlTT -> do
+      prods <- forM (source^.dsFilePaths) $ \fn -> do
+        let tName = read . reverse . dropWhile (/= '.') . drop 3 . reverse $ fn :: TrodeName
+        (getMWLSpikeProducer fn tName)
+      return . DSSpikes $ DataSource (catMaybes prods) q p
+    MwlP  -> do
+      prod <- getMWLPProducer cfg (
+
+{-
+          (posTime, getMWLPProducer)
+      x = fileSpecifics :: (a -> ExperimentTime, Conf.Config -> FilePath -> IO(Maybe(P.Producer a IO ())))
+  let node = source ^. dsNode
       format = Main.Binary
       tTarg = mock ^. mockFirstSeekTime
-      producecs = 
-      prods' = map (>-> dropWhile (\a -> timeFun a < tTarg)) producers
-  return $ DataSource (seq prods' prods') q timeFun p node format
+  case fileSpecifics of
+    (timeFun, Just getProducer) -> do
+      prods' <- forM (source^.dsFilePaths) $ \fn -> do
+        producer <- getProducer fn
+        (producer >-> Prelude.dropWhile (\a -> timeFun a < tTarg))
+      return $ DataSource (seq prods' prods') q timeFun p node format
+-}
 
+getMWLSpikeProducer :: FilePath -> TrodeName ->
+                       IO (Maybe (P.Producer TrodeSpike IO ()))
+getMWLSpikeProducer fn tName = do
+  f  <- BSL.readFile fn
+  fi' <- getFileInfo fn
+  case fi' of
+    Right fi -> 
+      return $ Just (dropResult (produceTrodeSpikes tName fi f ))
+    Left _ -> return Nothing
+
+getMWLPProducer :: Conf.Config -> FilePath ->
+                   IO (Maybe (P.Producer Position IO ()))
+getMWLPProducer cfg fp = do
+  hints' <- runMaybeT $ do
+    pxX <- MaybeT $ Conf.lookup cfg "mwlPHints.originXPixel"
+    pxY <- MaybeT $ Conf.lookup cfg "mwlPHints.originXPixel"
+    pS  <- MaybeT $ Conf.lookup cfg "mwlPHints.pxPerMeter"
+    pH  <- MaybeT $ Conf.lookup cfg "mwlPHints.trackHeight"
+    return $ PosMWLShim pxX pxY pS pH
+  case hints' of
+    Nothing -> return Nothing
+    Just hints -> return . Just $ producePosition hints fp
 
 ------------------------------------------------------------------------------
-runDataSource :: MockState -> DataSource a -> IO ()
-runDataSource opts ds = do
-  inThread  <- async $ (atomically $ readTVar (opts^.mockStatus) >>= \r ->
+runDataSource :: Timestamp a => MockOpts -> MockState -> DataSourceOpts ->
+                 DataSource a-> IO ()
+runDataSource opts mockS dsOpt dsState = do
+  inThread  <- async $ (atomically $ readTVar (mockS^.mockStatus) >>= \r ->
                          unless (r == Waiting) retry)
                >> runInput
   outThread <- async $ runOutput
@@ -145,26 +211,26 @@ runDataSource opts ds = do
     where
       runInput = do
         tTarget <- spoolToTime
-        forM_ (ds^.dsProducers) $ \p ->
-          P.runEffect $ P.for (p >-> PP.takeWhile (\a -> (ds^.dsGetTS $ a) <= tTarget))
-          (\a -> lift . atomically . modifyTVar (ds^.dsWaiting) $
-                 M.insert (ds^.dsGetTS $ a) a)
+        forM_ (dsState^.dsProducers) $ \p ->
+          P.runEffect $ P.for (p >-> PP.takeWhile (\a -> (getTS a) <= tTarget))
+          (\a -> lift . atomically . modifyTVar (dsState^.dsWaiting) $
+                 M.insert (getTS a) a)
 
       runOutput = P.runEffect $
-                  P.for (outProducer >-> relativeTimeCat (ds^.dsGetTS))
-                  (lift . atomically . writeTQueue (ds^.dsPub.chan))
+                  P.for (outProducer >-> relativeTimeCat getTS)
+                  (lift . atomically . writeTQueue (dsState^.dsPub.chan))
       outProducer = forever $ do
         v <- lift $ atomically $ do
-          m <- readTVar $ ds^.dsWaiting
+          m <- readTVar $ dsState^.dsWaiting
           when (M.null m) retry
           return $ snd . M.elemAt 0 $ m
         P.yield v
 
       spoolToTime :: IO ExperimentTime
       spoolToTime = do
-        f <- atomically $ readTVar (opts^.mockWallTimeFun)
         t <- getCurrentTime
-        return $ f t + opts^.readAheadSec
+        f <- readTVarIO $ mockS^.mockWallTimeFun
+        return $ f t + opts^.mockReadAheadTime
       
             
   
