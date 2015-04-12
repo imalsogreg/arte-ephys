e4455668aee466ee219c7acb22c1af67cf03c97d
Shea Levy <shea@shealevy.com>
2015-04-11 20:46:05 -0400

    Add nix expressions for arte-decoder

--- shell-common.nix
+++ shell-common.nix
@@ -1,7 +1,8 @@
 let
   inherit ((import <nixpkgs> {}).pkgs.haskell-ng.packages.ghc7101.override {
-    overrides = self: super: {
-      tetrode-ephys = self.callPackage ./tetrode-ephys {};
-    };
-  }) callPackage;
+    overrides = self: super: builtins.listToAttrs (map (name: {
+        inherit name;
+        value = self.callPackage (./. + "/${name}") {};
+      }) [ "tetrode-ephys" "arte-lib" "tetrode-graphics" ]);
+    }) callPackage;
 in path: (callPackage path {}).env
