bab51a3676ade3c486748f6ec14fdf9e07cb0378
Shea Levy <shea@shealevy.com>
2015-07-15 13:55:48 -0400

    Document TrackPosition

--- tetrode-ephys/src/Data/Ephys/TrackPosition.hs
+++ tetrode-ephys/src/Data/Ephys/TrackPosition.hs
@@ -1,3 +1,12 @@
+{-|
+Module      : Data.Ephys.TrackPosition
+Description : Rat positions in decoding reference frame
+Copyright   : (c) 2015 Greg Hale, Shea Levy
+License     : BSD3
+Maintainer  : imalsogreg@gmail.com
+Stability   : experimental
+Portability : GHC, Linux
+-}
 {-# LANGUAGE TemplateHaskell, BangPatterns #-}
 
 module Data.Ephys.TrackPosition where
@@ -16,40 +25,66 @@ import Data.Ephys.Position
 
 
 ------------------------------------------------------------------------------
+-- | A physical track segment
 data TrackBin =
-  TrackBin { _binName :: !String
+  TrackBin { -- | Identifier for the bin to define trajectories
+             _binName :: !String
+             -- | Origin of the track bin in room coordinates
            , _binLoc  :: !Location
+             -- | The outgoing direction, in radians
            , _binDir  :: !Double -- radians
+             -- | Distance from the origin to the beginning of the bin
+             --   in meters
            , _binA    :: !Double
+             -- | Distance from the origin to the end of the bin in meters
            , _binZ    :: !Double
+             -- | The width (i.e. the dimension perpendicular to the outgoing
+             --   direction) of the bin, in meters.
            , _binWid  :: !Double
+             -- | The shape of the bin
            , _binCaps :: !BinCaps
            } deriving (Eq, Ord, Show)
 
-data BinCaps = CapCircle
+-- | A bin shape
+data BinCaps = CapCircle -- ^ A circular bin for choice points on the track
+               -- | Rectangular bins for paths along the track. Parameter is
+               --   a tuple of the angle between the bin and its predecessor and
+               --   the angle between the bin and its successor.
              | CapFlat (Double,Double)
              deriving (Eq, Ord, Show)
 
 $(makeLenses ''TrackBin)
 
-data Track = Track { _trackBins  :: [TrackBin]
+-- | A representation of the track
+data Track = Track { _trackBins  :: [TrackBin] -- ^ The bins of the track
                    } deriving (Eq, Show)
 
-data TrackDirection = Outbound | Inbound
-                    deriving (Eq, Ord, Show)
-
-data TrackEccentricity = OutOfBounds | InBounds 
-                       deriving (Eq, Ord, Show)
-
-data TrackPos = TrackPos { _trackBin :: !TrackBin
+-- | A position on the track suited for decoding analysis
+data TrackPos = TrackPos { -- | The bin the position is in
+                           _trackBin :: !TrackBin
+                           -- | The direction the rat is going in relative to
+                           --   the bin
                          , _trackDir :: !TrackDirection
+                           -- | Whether the rat is in bounds or not
                          , _trackEcc :: !TrackEccentricity
                          } deriving (Eq, Ord, Show)
+
+-- | A direction on the track relative to a given bin's direction
+data TrackDirection = Outbound -- ^ In line with the bin's direction
+                    | Inbound -- ^ Against the bin's direction
+                    deriving (Eq, Ord, Show)
+
+-- | Inside or outside of the track
+data TrackEccentricity = OutOfBounds  -- ^ Out of the track's bounds
+                       | InBounds -- ^ In the track's bounds
+                       deriving (Eq, Ord, Show)
 
 $(makeLenses ''Track)
 $(makeLenses ''TrackPos)
 
-allTrackPos :: Track -> V.Vector TrackPos
+-- | Generate track positions from a track
+allTrackPos :: Track -- ^ The track of interest
+            -> V.Vector TrackPos
 allTrackPos t =
   V.fromList [TrackPos bin dir ecc | bin <- t^.trackBins
                                    , dir <- [Inbound,Outbound]
@@ -58,19 +93,29 @@ allTrackPos t =
 -- Use mapping from track bin to a to model 'fields' in general
 -- ie an instantaneous occupancy field, a trial-sum occupancy
 -- field, or a spike rate field
+-- | A real-valued quantity associated with a track by position
 type Field = V.Vector Double
 
+-- | A 'Field' where each value is paired with its associated position
 type LabeledField a = V.Vector (TrackPos, a)
 
-labelField :: Track -> Field -> LabeledField Double
+-- | Label a 'Field' with respect to a given 'Track'
+labelField :: Track -- ^ The track to label with respect to
+           -> Field -- ^ The field to label
+           -> LabeledField Double
 labelField t f = V.zip (allTrackPos t) f
 
-data PosKernel = PosDelta
+-- ^ Smoothing kernel for camera position
+data PosKernel = PosDelta -- ^ Delta function
+                 -- | Gaussian smoothing. Parameter is standard deviation
                | PosGaussian Double
 
 ------------------------------------------------------------------------------
--- Turn a position into an instantaneous field
-posToField :: Track -> Position -> PosKernel -> Field
+-- | Turn a position into an instantaneous field
+posToField :: Track -- ^ The track the position is on
+           -> Position -- ^ The position we're transforming
+           -> PosKernel -- ^ The smoothing kernel to use
+           -> Field
 posToField t pos kern =
     let distSq bin = locSqDist (pos^.location) (bin^.binLoc)
         binC       = trackClosestBin t pos
@@ -100,7 +145,10 @@ posToField t pos kern =
      in (V.map trackPosVal (allTrackPos t))
 
 ------------------------------------------------------------------------------
-relativeCoords :: TrackBin -> (Double,Double) -> (Double,Double)
+-- | Translate from room coordinates to track bin coordinates
+relativeCoords :: TrackBin -- ^  The bin for the final reference frame
+               -> (Double,Double) -- ^ The location in room coordinates
+               -> (Double,Double)
 relativeCoords bin (x',y') =
   let th = (-1 * bin^.binDir)
       dx = x' - bin^.binLoc.x
@@ -108,20 +156,27 @@ relativeCoords bin (x',y') =
   in
    (dx * cos th - dy * sin th, dx * sin th + dy * cos th)
 
-trackClosestBin :: Track -> Position -> TrackBin
+-- | Find the closest bin to a given position.
+trackClosestBin :: Track -- ^ The track the position is on
+                -> Position -- ^ The position to find a bin near
+                -> TrackBin
 trackClosestBin track pos =
   head . sortBy (comparing (posBinDistSq pos)) $
   (track ^. trackBins)
 
-posBinDistSq :: Position -> TrackBin -> Double
+-- | Squared distance between a position and the center of a bin, in m^2
+posBinDistSq :: Position -- ^ The position
+             -> TrackBin -- ^ The bin
+             -> Double
 posBinDistSq pos bin = locSqDist (bin^.binLoc)
                        (pos^.location)
 
-circularTrack :: (Double,Double) -- (x,y) in meters
-                 -> Double       -- radius in meters
-                 -> Double       -- height in meters
-                 -> Double       -- track width in meters
-                 -> Double       -- bin length in meters
+-- | Create a circular track
+circularTrack :: (Double,Double) -- ^ Center of the track in room coordinates in meters
+                 -> Double       -- ^ radius in meters
+                 -> Double       -- ^ height in meters
+                 -> Double       -- ^ track width in meters
+                 -> Double       -- ^ bin length in meters
                  -> Track
 circularTrack (cX,cY) r h w tau =
   Track [aPoint t [n] | (t,n) <- zip thetaCs names]
@@ -142,7 +197,11 @@ circularTrack (cX,cY) r h w tau =
       w
       (CapFlat (thetaIncr/(-2), thetaIncr/2))
 ------------------------------------------------------------------------------
-updateField :: (Double->Double->Double) -> Field -> Field -> Field
+-- | Zip a function over two fields
+updateField :: (Double->Double->Double) -- ^ The function to combine each pair of values
+            -> Field -- ^ The first field
+            -> Field -- ^ The second field
+            -> Field
 updateField = V.zipWith
 {-# INLINE updateField #-}
 
