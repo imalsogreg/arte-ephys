2e735df83c32fbb56e6b1a1ac4f9ed496b9d6647
Shea Levy <shea@shealevy.com>
2015-07-24 14:53:42 -0400

    Document Net

--- arte-lib/src/System/Arte/Net.hs
+++ arte-lib/src/System/Arte/Net.hs
@@ -1,5 +1,13 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell, DeriveGeneric #-}
-
+{-|
+Module      : System.Arte.Net
+Description : Miscellaneous networking utilities
+Copyright   : (c) Greg Hale, 2015
+                  Shea Levy, 2015
+License     : BSD3
+Maintainer  : imalsogreg@gmail.com
+Stability   : experimental
+Portability : GHC, Linux
+-}
 module System.Arte.Net where
 
 import Control.Monad.IO.Class (liftIO)
@@ -9,7 +17,10 @@ import qualified Network.Socket.ByteString as BS
 import Network
 import System.IO
 
-udpSocketProducer :: (Serialize t) => Int -> Socket -> Producer t IO ()
+-- | Produce a stream of ts by reading them from a socket forever
+udpSocketProducer :: (Serialize t) => Int -- ^ Buffer size for packets
+                                   -> Socket -- ^ Socket to listen on
+                                   -> Producer t IO ()
 udpSocketProducer buffSize s = forever $ do
   (buf, _) <- liftIO $ BS.recvFrom s buffSize
   case decode buf of
