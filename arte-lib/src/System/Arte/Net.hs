3347e1da921351eafa01eefd83f3a8bb984399b9
Greg Hale <imalsogreg@gmail.com>
2015-08-18 15:45:17 -0400

    more decoding streams

--- arte-lib/src/System/Arte/Net.hs
+++ arte-lib/src/System/Arte/Net.hs
@@ -12,18 +12,40 @@ module System.Arte.Net where
 
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.Aeson as A
 import Pipes
 import Data.Serialize
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
 import qualified Network.Socket.ByteString as BS
 import Network
 import System.IO
 
--- | Produce a stream of ts by reading them from a socket forever
+
+-- | Produce a stream of Serializable ts by reading them from a socket forever
 udpSocketProducer :: (Serialize t) => Int -- ^ Buffer size for packets
                                    -> Socket -- ^ Socket to listen on
                                    -> Producer t IO ()
-udpSocketProducer buffSize s = forever $ do
+udpSocketProducer = udpSocketProducerWith decode
+
+
+
+-- | Produce a stream of FromJSON ts by reading them from a socket forever
+udpJsonProducer :: (A.FromJSON t) => Int -- ^ Buffer size for packets
+                                  -> Socket -- ^ Socket to listen on
+                                  -> Producer t IO ()
+udpJsonProducer = udpSocketProducerWith (A.eitherDecode . BL.fromStrict)
+
+
+-- | Produce o stream of ts by reading from a socket forever,
+--   with a custom conversion function
+udpSocketProducerWith :: (BS.ByteString -> Either String t)
+                      -- ^ Conversion from bytes to t
+                      -> Int                  -- ^ Maximum buffer length
+                      -> Socket               -- ^ Incoming data socket
+                      -> Producer t IO ()
+udpSocketProducerWith f buffSize s = forever $ do
   (buf, _) <- liftIO $ BS.recvFrom s buffSize
-  case decode buf of
+  case f buf of
     Left e -> liftIO $ hPutStrLn stderr ("Error parsing packet: " ++ e)
     Right t -> yield t
