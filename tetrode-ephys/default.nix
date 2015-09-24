6519d3de10a2734bb8d4b009eb9c8e3ff10be273
Shea Levy <shea@shealevy.com>
2015-09-24 14:43:33 -0400

    tetrode-ephys: Bump version bounds, update default.nix

--- tetrode-ephys/default.nix
+++ tetrode-ephys/default.nix
@@ -1,16 +1,19 @@
-{ mkDerivation, async, base, binary, bytestring, cereal, containers
-, data-binary-ieee754, lens, parsec, pipes, pipes-binary
-, pipes-bytestring, pipes-parse, pipes-rt, safe, safecopy, stdenv
-, stm, text, time, vector, vector-binary-instances
+{ mkDerivation, aeson, async, base, binary, bytestring, cereal
+, containers, data-binary-ieee754, lens, parsec, pipes
+, pipes-binary, pipes-bytestring, pipes-parse, pipes-rt, safe
+, safecopy, stdenv, stm, text, time, vector
+, vector-binary-instances
 }:
 mkDerivation {
   pname = "tetrode-ephys";
   version = "0.1.0.0";
   src = ./.;
-  buildDepends = [
-    async base binary bytestring cereal containers data-binary-ieee754
-    lens parsec pipes pipes-binary pipes-bytestring pipes-parse
-    pipes-rt safe safecopy stm text time vector vector-binary-instances
+  libraryHaskellDepends = [
+    aeson async base binary bytestring cereal containers
+    data-binary-ieee754 lens parsec pipes pipes-binary pipes-bytestring
+    pipes-parse pipes-rt safe safecopy stm text time vector
+    vector-binary-instances
   ];
+  description = "Types and functions for describing neurons and place fields";
   license = stdenv.lib.licenses.bsd3;
 }
