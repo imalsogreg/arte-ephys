989ccb601374f2ac7c9f0e146f3e95d2f5b6316d
Shea Levy <shea@shealevy.com>
2015-10-01 12:26:59 -0400

    Generalize script

--- scripts/simpleDemo.sh
+++ scripts/simpleDemo.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-DATADIR="/home/greghale/Data/caillou/112812clip2"
+DATADIR="$HOME/data/caillou/112812clip2"
 STARTTIME="4492"
 
 echo "Starting arte-time-sync"
 ../arte-time-sync/time &
 
 echo "Starting mock data"
-../arte-mock-data/dist/build/arte-mock-data/arte-mock-data --format ArteJSON --localport 5000 --destport 5001 --startTime 4492 --file anything.pfake
+../arte-mock-data/dist/build/arte-mock-data/arte-mock-data --format ArteJSON --localport 5000 --destport 5001 --startTime 4492 --file anything.pfake &
 
 echo "Starting trode decoder"
-../arte-decoder/dist/build/trode-decoder/trode-decoder --ttDir $HOME/data/caillou/112812clip2/1028/ --timeSyncPort 6080 --posport 5001 --spikeformat ArteJSON
-echo "Starting ArteDecode"
+../arte-decoder/dist/build/trode-decoder/trode-decoder --ttDir $DATADIR/1028/ --timeSyncPort 6080 --posport 5001 --spikeformat ArteJSON
 
