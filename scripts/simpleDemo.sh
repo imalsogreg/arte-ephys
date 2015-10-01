dc2dbe3aa9e6c03e43ba6ac9629ad0ce07cd739d
Greg Hale <imalsogreg@gmail.com>
2015-10-01 12:18:43 -0400

    update demo script

--- scripts/simpleDemo.sh
+++ scripts/simpleDemo.sh
@@ -3,11 +3,13 @@
 DATADIR="/home/greghale/Data/caillou/112812clip2"
 STARTTIME="4492"
 
-echo "Starting ArteMaster"
-.cabal-sandbox/bin/arte-master -s=2 &
-
-echo "Starting ArteDecode"
-.cabal-sandbox/bin/arteDecode &
+echo "Starting arte-time-sync"
+../arte-time-sync/time &
 
 echo "Starting mock data"
-.cabal-sandbox/bin/arteMockSpikes -i=false --startexperimenttime=$STARTTIME --basedirectory=$DATADIR --searchdepth=2 &
+../arte-mock-data/dist/build/arte-mock-data/arte-mock-data --format ArteJSON --localport 5000 --destport 5001 --startTime 4492 --file anything.pfake
+
+echo "Starting trode decoder"
+../arte-decoder/dist/build/trode-decoder/trode-decoder --ttDir $HOME/data/caillou/112812clip2/1028/ --timeSyncPort 6080 --posport 5001 --spikeformat ArteJSON
+echo "Starting ArteDecode"
+
