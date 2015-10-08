87a65526a470a1650aef7d6bcf14ccf756f98286
Shea Levy <shea@shealevy.com>
2015-10-08 11:30:52 -0400

    time-sync: Clean up conditional compliation

--- arte-time-sync/arte-time-sync.c
+++ arte-time-sync/arte-time-sync.c
@@ -43,17 +43,16 @@ int main(int argc, char ** argv) {
 
 
 	do {
-		struct timespec time;
-
 #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    time.tv_sec = mts.tv_sec;
-    time.tv_nsec = mts.tv_nsec;
+		clock_serv_t cclock;
+		mach_timespec_t time;
+		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+		clock_get_time(cclock, &mts);
+		mach_port_deallocate(mach_task_self(), cclock);
+		time.tv_sec = mts.tv_sec;
+		time.tv_nsec = mts.tv_nsec;
 #else
+		struct timespec time;
 		if (clock_gettime(CLOCK_MONOTONIC, &time) == -1) {
 			perror("getting monotonic time");
 			continue;
